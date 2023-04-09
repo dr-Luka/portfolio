458fd0a2bc212af8dfe3bac7b6325ea57e38aea2
dr-Luka <63805178+dr-Luka@users.noreply.github.com>
2023-04-09 21:53:26 +0200

    Portfolio

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./styles/style.scss";
 import Nav from "./components/Nav.js";
 import Home from "./components/Home.js";
 import About from "./components/About.js";
+import Portfolio from "./components/Portfolio";
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +11,7 @@ function App() {
         <Nav />
         <Home />
         <About />
+        <Portfolio />
       </div>
     </div>
   );
---
+++ src/components/Portfolio.js
@@ -0,0 +1,48 @@
+export default function Portfolio() {
+  return (
+    <>
+      <div className="section portfolio" id="portfolio">
+        <div className="h2-bg">PORTFOLIO</div>
+        <h2>My Work</h2>
+        <div className="projects">
+          <div className="project">
+            <div className="project-img"></div>
+            <div className="project-info">
+              <div className="project-desc">
+                Lisque persius interesset his et, in quot quidam persequeris
+                vim, ad mea essent possim iriure.Lisque persius interesset his
+                et, in quot quidam persequeris vim, ad mea essent possim
+              </div>
+              <div className="project-links">
+                <a href="index" className="button buttonFill">
+                  Preview
+                </a>
+                <a href="index" className="button buttonFill">
+                  Code
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="project reverse">
+            <div className="project-img"></div>
+            <div className="project-info">
+              <div className="project-desc">
+                Lisque persius interesset his et, in quot quidam persequeris
+                vim, ad mea essent possim iriure.Lisque persius interesset his
+                et, in quot quidam persequeris vim, ad mea essent possim
+              </div>
+              <div className="project-links">
+                <a href="index" className="button buttonFill">
+                  Preview
+                </a>
+                <a href="index" className="button buttonFill">
+                  Code
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
