7f5b144fd6290d8b03ceb04c15a771613c51d80d
dr-Luka <63805178+dr-Luka@users.noreply.github.com>
2023-04-09 21:18:49 +0200

    Home/About 100%

--- src/components/About.js
+++ src/components/About.js
@@ -3,14 +3,14 @@ import stack from "../media/icons/stack.png";
 export default function About() {
   return (
     <>
-      <div className="section knowMe" id="about">
+      <div className="section about" id="about">
         <div className="h2-bg">ABOUT ME</div>
         <h2>Know Me More</h2>
         <div className="studies">
-          Graduated from Noroff University with Front-End Development as my
-          focus.
+          Graduated from <span className="accent">Noroff University</span> with{" "}
+          <span className="accent">Front-End Development</span> as my focus.
         </div>
-        <div className="flex">
+        <div className="info">
           <div className="techStack">
             <span>Tech Stack</span>
             <img src={stack} alt="Tech Stack" />
@@ -20,6 +20,10 @@ export default function About() {
             <div>Email: luka000mikic@gmail.com</div>
             <div>Age: 25</div>
             <div>Language: English</div>
+
+            <a href="index" className="button buttonFill">
+              Download CV
+            </a>
           </div>
         </div>
       </div>
